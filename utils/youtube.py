85c919719b3d988a1a5953814c80c786f818b223
Kamron201111 <xonsamir299@gmail.com>
2025-11-29 16:12:21 +0500

    Create youtube.py

---
+++ utils/youtube.py
@@ -0,0 +1,13 @@
+from youtubesearchpython import VideosSearch
+import yt_dlp
+
+def search_youtube(query):
+    v = VideosSearch(query, limit=1).result()['result'][0]
+    return {"title": v['title'], "url": v['link']}
+
+def download_audio(url):
+    out = 'music.mp3'
+    ydl = {'format': 'bestaudio', 'outtmpl': out}
+    with yt_dlp.YoutubeDL(ydl) as y:
+        y.download([url])
+    return out
